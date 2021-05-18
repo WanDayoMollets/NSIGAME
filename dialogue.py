f4294e36b7571ae0c46737771b1f608041e28d5b
iPauluxFX <66026801+iPauluxFX@users.noreply.github.com>
2021-05-18 09:52:36 +0200

    Add files via upload
    dialogue (text) fonction

--- dialogue.py
+++ dialogue.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import os
 import sys
+import time
 
 
 from pygame.rect import Rect
@@ -9,12 +10,11 @@ from pygame.sprite import collide_rect
 
 from pygame.constants import MOUSEBUTTONDOWN
 
-x,y=1000,640
+x,y=1280,960
 window_resolution = (x,y)
 launched = True
 mainClock = pygame.time.Clock()
 pygame.init()
-pygame.display.set_caption("Dialogue")
 screen = pygame.display.set_mode(window_resolution)
 fontMenu = pygame.font.SysFont(None, 100)
 fontMenuChoice = pygame.font.SysFont(None, 30)
@@ -26,19 +26,26 @@ def draw_text(text,font,color,surface,x,y):
     textrect.topleft = (x,y)
     surface.blit(textobj, textrect)
 
-def parlez():
+def parlez(texte,x,y):
     a = True
-    while a:
-        screen.fill((0,0,0))
-
-        draw_text("Prof Chen", fontMenu, (255,255,255), screen, x*0.345,y*0.2)
-        mx, my = pygame.mouse.get_pos()
-        dialogue = pygame.Rect(x*0.5,y*0.5,x*0.2,y*0.125)
-        pygame.draw.rect(screen, (0,0,255), dialogue)
-        draw_text("Hello", fontMenuChoice, (0,255,0), screen, x*0.27,y*0.55)
-
-        click = False
+    b= True
+    i=0
     
+    while a:
+        
+        mx, my = pygame.mouse.get_pos()
+
+        if b:
+            i+=1
+            texteP(texte,i,x,y)
+            if i >= len(texte):    
+                b=False
+            time.sleep(0.03)
+        if not b:
+            if event.type == MOUSEBUTTONDOWN:
+                    if click:
+                        a = False
+            
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -50,6 +57,11 @@ def parlez():
         pygame.display.update()
         mainClock.tick(60)
 
+def texteP(texte,i,x,y):
+        draw_text(texte[:i], fontMenuChoice, (255,255,255), screen, x,y)
+
+#test
+"""
 def main():    
     while True:
         screen.fill((0,0,0))
@@ -60,7 +72,7 @@ def main():
         dialogue = pygame.Rect(x*0.2,y*0.5,x*0.2,y*0.125)
         if dialogue.collidepoint((mx,my)):
             if click:
-                parlez()
+                parlez(texte,300,400)
         pygame.draw.rect(screen, (255,0,0), dialogue)
         draw_text("Parlez", fontMenuChoice, (255,255,255), screen, x*0.27,y*0.55)
 
@@ -76,6 +88,11 @@ def main():
                         
         pygame.display.update()
         mainClock.tick(60)
-    
 
+
+texte= 'Salut petite merde !'
 main()
+        
+"""        
+
+    
