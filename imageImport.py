6d0098e02f676b7f7e6d3f1d124fd9223d80d032
WanDayoMollets <65540277+WanDayoMollets@users.noreply.github.com>
2021-06-03 09:33:47 +0200

    Add files via upload
    ajout des commentaires

--- imageImport.py
+++ imageImport.py
@@ -1,27 +1,28 @@
 import requests
 import shutil
 
-def importPokemonImage(pokemonName,playerType):
-    replaceChar = " "
-    pokemonName = pokemonName.replace(replaceChar,"-")
-    if playerType == "joueur":
-        image_url = "https://img.pokemondb.net/sprites/black-white/back-normal/"+pokemonName+".png"
-        filename = "cache/joueur/currentPokemon.png"
-    elif playerType == "IA":
-        image_url = "https://img.pokemondb.net/sprites/black-white/normal/"+pokemonName+".png"
-        filename = "cache/IA/currentPokemon.png"
-    elif playerType == "start":
-        image_url = "https://img.pokemondb.net/sprites/black-white/normal/"+pokemonName+".png"
-        filename = "cache/start/"+pokemonName+".png"
+def importPokemonImage(pokemonName,playerType): 
+    """Importe le sprite d'un pokemon en fonction de son nom, il est enregistré dans un dossier différent en fonction du type de joueur"""
+    replaceChar = " " #indique que le caractere à remplacer est l'espace
+    pokemonName = pokemonName.replace(replaceChar,"-") #remplace les espaces dans le nom d'un pokemon par des tirets 
+    if playerType == "joueur": #si le type de joueur est joueur
+        image_url = "https://img.pokemondb.net/sprites/black-white/back-normal/"+pokemonName+".png" #indique l'url de l'image en fonction du nom du pokemon
+        filename = "cache/joueur/currentPokemon.png" #indique qu'il faut enregistrer le sprite dans le cache du joueur
+    elif playerType == "IA": #si le type de joueur est IA
+        image_url = "https://img.pokemondb.net/sprites/black-white/normal/"+pokemonName+".png" #indique l'url de l'image en fonction du nom du pokemon
+        filename = "cache/IA/currentPokemon.png" #indique qu'il faut enregistrer le sprite dans le cache de l'IA
+    elif playerType == "start": #si le type de joueur est start
+        image_url = "https://img.pokemondb.net/sprites/black-white/normal/"+pokemonName+".png" #indique l'url de l'image en fonction du nom du pokemon
+        filename = "cache/start/"+pokemonName+".png" #indique qu'il faut enregistrer le sprite dans le cache du start
 
-    r = requests.get(image_url, stream = True)
+    r = requests.get(image_url, stream = True) #creer une requete pour recuperer l'image
 
-    if r.status_code == 200:
-        r.raw.decode_content = True
+    if r.status_code == 200: #si l'image peut-être décodée
+        r.raw.decode_content = True #décode l'image 
 
-        with open(filename,'wb') as f:
-            shutil.copyfileobj(r.raw, f)
+        with open(filename,'wb') as f: #ouvre l'image
+            shutil.copyfileobj(r.raw, f) #copie de l'image dans le dossier
             
-        print('Image sucessfully Downloaded: ',filename)
-    else:
-        print('Image Couldn\'t be retreived')
+        print("L'image a été correctement téléchargée :",filename)
+    else: #si l'image ne peut-être décodée
+        print("L'image ne peut pas être téléchargée")
